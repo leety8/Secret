9866f77cb22e7e460a22cd2ac625f356a04dc9d8
Luckson Dor <lr.dor86@gmail.com>
2022-10-07 11:41:32 -0400

    check my error on local

--- app.js
+++ app.js
@@ -3,35 +3,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-//const { Console } = require("console");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-//log the API_key
-console.log(process.env.API_KEY);
-
+app.use(express.static("public"));
 app.set("view engine", "ejs");
-
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
-app.use(express.static("public"));
-
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+//using those packages
+app.use(passport.initialize());
+app.use(passport.session());
 //Connect the server to hte mongodb server
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 //set up a new DB with schema first
-const userSchema = new mongoose.Schema({ email: "String", password: "String" });
-//To have the secret from .env file
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
-//create the model to create users to add userDB to
+const userSchema = new mongoose.Schema({ email: "String", passport: "String" });
+
+userSchema.plugin(passportLocalMongoose);
+//userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //to see the pages
 app.get("/", function (req, res) {
@@ -43,38 +49,32 @@ app.get("/login", function (req, res) {
 app.get("/register", function (req, res) {
   res.render("register");
 });
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.render("/login");
+  }
+});
 // get the user info
 app.post("/register", function (req, res) {
-  //grasb the name on the body name variable htlm
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
-
-  newUser.save(function (err) {
-    if (err) {
-      Console.log(err);
-    } else {
-      res.render("Secrets");
-    }
-  });
-});
-//to log if there are an account alreay check on the database
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("Secrets");
-        }
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
       }
     }
-  });
+  );
 });
+//to log if there are an account alreay check on the database
+app.post("/login", function (req, res) {});
 
 //TODO r
 app.listen(3000, function () {
